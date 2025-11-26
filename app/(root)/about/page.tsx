769ccd242d0a92994a7b529519840c7fbfe62fb5
Yashraj_K <yashrajkamble7727@gmail.com>
2025-11-26 19:32:39 +0530

    Routing

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const About = () => {
+    throw new Error("Test error in about page");
+    return (
+        <div>About      </div>
+    )
+}
+
+export default About
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Hello from './componenets/hello';
 
 const Home = () => {
   console.log("what type of a component am I?");
   return (
     <main>
       <div className='text-5xl underline'>Welcome to Next.js!</div>
-      <Hello />
     </main>
   )
 }
